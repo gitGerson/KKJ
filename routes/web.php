81ab7640046a114529ae6e9a0cd53d007b3e7d81
gitGerson <dev.gersonmanuel@gmail.com>
2026-05-19 00:41:17 +0700

    Refactor code structure for improved readability and maintainability

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'welcome')->name('home');
+Route::get('/', fn () => to_route('dashboard'))->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::view('dashboard', 'dashboard')->name('dashboard');
