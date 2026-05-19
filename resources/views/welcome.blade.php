61357536e9d75621985d1338d2b47b91c4c4f1aa
gitGerson <dev.gersonmanuel@gmail.com>
2026-05-20 02:23:30 +0700

    Add font assets and manifest files for Instrument Sans
    - Created manifest.json to manage font and asset files.
    - Added fonts-manifest.dev.json for development font management.
    - Introduced hot file for Vite server configuration.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,8 +6,8 @@
 
         <title>{{ __('Welcome') }} - {{ config('app.name', 'Laravel') }}</title>
 
-        <link rel="icon" href="/favicon.ico" sizes="any">
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml">
+        <link rel="icon" href="/logo.png" sizes="any">
+        <link rel="icon" href="/logo.png" type="image/svg+xml">
         <link rel="apple-touch-icon" href="/apple-touch-icon.png">
 
         @fonts
